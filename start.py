b5450aefa7ca4ecbf2ad20a3f19424d04d3a1785
wiyantoro <wiyantoro.kirlan@gmail.com>
2020-04-20 17:33:32 +0700

    ullalalala

--- start.py
+++ start.py
@@ -22,10 +22,4 @@ t_images = np.hstack((t_bias,t_images))
 num_class=len(np.unique(labels))
 
 thetas=np.random.rand(m,n-1)*0,001
-#print(thetas)
-
-<<<<<<< HEAD
-#############################################################
-=======
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
->>>>>>> 9290b80b37a0a193a53caace5c7885040cdb0190
+print(thetas)
